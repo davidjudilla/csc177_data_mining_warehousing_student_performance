c846798b61d81a31b4c995a6230a95de62c7defb
David Judilla <davidfjudilla@gmail.com>
2017-04-27 17:05:14 -0700

    added average of numberic values for gradeAvgStats

--- server/server.py
+++ server/server.py
@@ -12,23 +12,27 @@ allStudentsConn = sqlite3.connect('unionedStudents.db')
 allStudCur = allStudentsConn.cursor()
 
 # columsn for gradeAvgStats to check
-statsColumns = [
+mostOccuringStatsCols = [
 	"sex",
 	"traveltime",
 	"studytime",
-	"failures",
 	"schoolsup",
 	"famsup",
 	"activities",
 	"nursery",
 	"internet",
-	"romantic",
+	"romantic"
+]
+avgStatsCols = [
+	"age",
+	"absences",
+	"failures",
 	"famrel",
 	"freetime",
 	"goout",
-	"wAlc",
-	"health",
-	"absences"
+	"Dalc",
+	"Walc",
+	"health"
 ]
 
 def getMostOccuring(cur, grade, col):
@@ -43,6 +47,15 @@ def getMostOccuring(cur, grade, col):
 	# cur.execute('SELECT * FROM Students WHERE G3 = ?');
 	return cur.fetchone()[0]
 
+def getAverageOfCol(cur, grade, col):
+	cur.execute("""
+		SELECT {0}, AVG({0})
+		FROM Students
+		WHERE G3 = {1}
+	""".format(col, grade))
+	# cur.execute('SELECT * FROM Students WHERE G3 = ?');
+	return cur.fetchone()[1]
+
 
 # Routes
 @app.route("/")
@@ -53,8 +66,10 @@ def hello():
 def gradeAvgStats():
 	grade = request.args.get('grade')
 	stats = {}
-	for x in statsColumns:
+	for x in mostOccuringStatsCols:
 		stats[x] = getMostOccuring(allStudCur, grade, x)
+	for x in avgStatsCols:
+		stats[x] = getAverageOfCol(allStudCur, grade, x)
 
 	print(stats)
 

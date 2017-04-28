3798fbda8e4e68ca592a3f457a3231367cccbd09
bryceLH <blh22buck@gmail.com>
2017-04-27 17:45:43 -0700

    charts added to webapp

--- web-app/app.js
+++ web-app/app.js
@@ -1,38 +1,70 @@
-angular.module('studentPerformance', ['angularCharts']);
+angular.module('studentPerformance', ['angularCharts'])
+        .controller('MainCtrl', [function ($scope){
 
-function MainCtrl($scope) {
-  $scope.config = {
-    title: 'Grades',
-    tooltips: true,
-    labels: false, //shows value on each bar
-    mouseover: function() {},
-    mouseout: function() {},
-    click: function() {},
-    legend: {
-      display: true,
-      position: 'right' //left || right
-    }
-  };
+            var vm = this;
+            vm.barGraph = undefined;
 
-  $scope.bartest = {
-    series: ['Trimester 1', 'Trimester 2', 'Trimester 3'],
-    data: [{
-      x: "F",
-      y: [10, 5, 1],
-      tooltip: "Grade 1: F"
-    }, {
-      x: "D",
-      y: [3, 1, 7]
-    }, {
-      x: "C",
-      y: [3, 5, 1]
-    }, {
-      x: "B",
-      y: [4, 2, 2]
-    }, {
-      x: ["A"],
-      y: [3, 5, 10]
-    }
-    ]
-  };
-}
+            vm.config = {
+              title: 'Grades',
+              tooltips: true,
+              labels: true, //shows value on each bar
+              mouseover: function() {},
+              mouseout: function() {},
+              click: function() {},
+              legend: {
+                display: true,
+                position: 'right' //left || right
+                }
+            };
+
+            vm.barGraph = {
+                series: ['Overall Grade'],
+                data: [{
+                    x: 1,
+                    y: [1],
+                    tooltip: "A"
+                },{
+                    x: 2,
+                    y: [2],
+                    tooltip: "B"
+                },{
+                    x: 3,
+                    y: [3],
+                    tooltip: "C"
+                },{
+                    x: 4,
+                    y: [4],
+                    tooltip: "D"
+                },{
+                    x: 5,
+                    y: [5],
+                    tooltip: "F"
+                }
+                ]
+            };
+
+                vm.bartest = {
+                series: ['Trimester 1', 'Trimester 2', 'Trimester 3'],
+                data: [{
+                  x: "A",
+                  y: [10, 5, 1],
+                  tooltip: "Grade 1: A"
+                }, {
+                  x: "B",
+                  y: [3, 1, 7]
+                }, {
+                  x: "C",
+                  y: [3, 5, 1]
+                }, {
+                  x: "D",
+                  y: [4, 2, 2]
+                }, {
+                  x: ["F"],
+                  y: [3, 5, 10]
+                }
+                ]
+              };
+
+
+
+        }]);

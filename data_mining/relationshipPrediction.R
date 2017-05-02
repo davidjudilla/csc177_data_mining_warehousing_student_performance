ee36b7d0f3d311dbe4db19b3d767bfea17a84e02
David Judilla <davidfjudilla@gmail.com>
2017-05-02 10:49:47 -0700

    progress on data mining

---
+++ data_mining/relationshipPrediction.R
@@ -0,0 +1,50 @@
+source("data_mining/preprocessing.R")
+
+# Questions
+# Is there a correlation between study time and final grade
+# Which factor has the greatest effect on the final grade
+  # Answer with bar chart
+
+# Is there a correlation between distractions (internet, going out) and final grade
+# Do after school activities effect students grades? What're some edge cases (ie does tutoring count as after school activites)
+# Do those in relationships have different grades than those without
+# What is the effect of nursery/preschool on a students grades
+  # Yes
+# Do students with school/family educational support have improved grades over other students
+
+library(caTools)
+set.seed(123)
+split = sample.split(unionStud, SplitRatio = 0.8)
+training_set = subset(unionStud, split == T)
+test_set = subset(unionStud, split == F)
+#Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+#Coming up Simple Linear Regression
+
+library(randomForest)
+# fit <- randomForest(G3 ~ . - id - G1 - G2, 
+#                     data = training_set,
+#                     importance = TRUE,
+#                     ntree = 5000
+# )
+
+fit <- randomForest(romantic ~ sex + age + famsize + traveltime + studytime + failures + 
+                      schoolsup + famsup + paid + activities + nursery + higher + internet +
+                      famrel + freetime + goout + Dalc + Walc + health + absences + G3, 
+                    data = training_set,
+                    importance = TRUE,
+                    ntree = 1000,
+                    do.trace = 100
+)
+
+varImpPlot(fit)
+print(fit)
+prediction <- predict(fit, test_set)
+final <- data.frame(id = test_set$id, actual = test_set$romantic, predictedG3 = prediction)
+
+
+library(ggplot2)
+ggplot(final) +
+  geom_point(aes(x = id, y = actual), color = "blue") + 
+  geom_point(aes(x = id, y = predictedG3), color = "red")

a837bbf1b78ce6e5d95403b3b9d60bcf66675494
David Judilla <davidfjudilla@gmail.com>
2017-05-02 19:03:56 -0700

    finished classifiers and regression

--- data_mining/relationshipPredictionCART.R
+++ data_mining/relationshipPredictionCART.R
@@ -3,13 +3,13 @@ source("data_mining/preprocessing.R")
 # Questions
 # Is there a correlation between study time and final grade
 # Which factor has the greatest effect on the final grade
-  # Answer with bar chart
+# Answer with bar chart
 
 # Is there a correlation between distractions (internet, going out) and final grade
 # Do after school activities effect students grades? What're some edge cases (ie does tutoring count as after school activites)
 # Do those in relationships have different grades than those without
 # What is the effect of nursery/preschool on a students grades
-  # Yes
+# Yes
 # Do students with school/family educational support have improved grades over other students
 
 library(caTools)
@@ -24,19 +24,19 @@ test_set = subset(unionStud, split == F)
 
 library(rpart)
 
-fit <- rpart(as.factor(studytime) ~ sex + age + famsize + traveltime + failures + 
-                      schoolsup + famsup + paid + activities + nursery + higher + internet + romantic +
-                      famrel + freetime + goout + Dalc + Walc + health + absences + G3, 
-                    data = training_set)
+fit <- rpart(romantic ~ sex + age + famsize + traveltime + studytime + failures + 
+               schoolsup + famsup + paid + activities + nursery + higher + internet +
+               famrel + freetime + goout + Dalc + Walc + health + absences + G3, 
+             data = training_set)
 
 print(fit)
 prediction <- predict(fit, test_set, type="class")
-result <- data.frame(id = test_set$id, actual = test_set$studytime, predictedTime = prediction)
-errorRate <- result[result$actual != result$predictedTime,] %>% nrow / nrow(result)
+results <- data.frame(id = test_set$id, actual = test_set$romantic, predictedTime = prediction)
+errorRate <- results[results$actual != results$predictedTime,] %>% nrow / nrow(results)
 acceptanceRate <- 1 - errorRate
 sprintf("Acceptance Rate: %f", acceptanceRate)
 
 library(ggplot2)
-ggplot(final) +
+ggplot(results) +
   geom_point(aes(x = id, y = actual), color = "blue") + 
   geom_point(aes(x = id, y = predictedTime), color = "red")
--- data_mining/relationshipPredictionRandomForest.R
+++ data_mining/relationshipPredictionRandomForest.R
@@ -17,26 +17,28 @@ set.seed(123)
 split = sample.split(unionStud, SplitRatio = 0.8)
 training_set = subset(unionStud, split == T)
 test_set = subset(unionStud, split == F)
-# Feature Scaling
+#Feature Scaling
 # training_set[, 2:3] = scale(training_set[, 2:3])
 # test_set[, 2:3] = scale(test_set[, 2:3])
 #Coming up Simple Linear Regression
 
-library(rpart)
-
-fit <- rpart(as.factor(studytime) ~ sex + age + famsize + traveltime + failures + 
-                      schoolsup + famsup + paid + activities + nursery + higher + internet + romantic +
+library(randomForest)
+fit <- randomForest(romantic ~ sex + age + famsize + traveltime + studytime + failures + 
+                      schoolsup + famsup + paid + activities + nursery + higher + internet +
                       famrel + freetime + goout + Dalc + Walc + health + absences + G3, 
-                    data = training_set)
+                    data = training_set,
+                    importance = TRUE,
+                    ntree = 1000,
+                    do.trace = 100
+)
 
+varImpPlot(fit)
 print(fit)
-prediction <- predict(fit, test_set, type="class")
-result <- data.frame(id = test_set$id, actual = test_set$studytime, predictedTime = prediction)
-errorRate <- result[result$actual != result$predictedTime,] %>% nrow / nrow(result)
-acceptanceRate <- 1 - errorRate
-sprintf("Acceptance Rate: %f", acceptanceRate)
+prediction <- predict(fit, test_set)
+final <- data.frame(id = test_set$id, actual = test_set$romantic, predicted = prediction)
+errorRate <- final[final$actual != final$predicted,] %>% nrow / final %>% nrow
 
 library(ggplot2)
 ggplot(final) +
   geom_point(aes(x = id, y = actual), color = "blue") + 
-  geom_point(aes(x = id, y = predictedTime), color = "red")
+  geom_point(aes(x = id, y = predicted), color = "red")
